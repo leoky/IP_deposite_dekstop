c6bf13b0276c53afd61f266c825bbf7fb6d8e12a
leoky <leonardy189@gmail.com>
2017-09-20 19:21:30 +0700

    already 1 add component 2 inserto db 3 a few validation 4 count days between two dates not yet : 1 count 2 validation

---
+++ src/DataBase.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Leonardy
+ */
+public class DataBase {
+
+    public static final String JDBC_CLASS = "com.mysql.jdbc.Driver";
+    public static final String JDBC_URL = "jdbc:mysql://localhost/ipi_dekstop";
+    public static final String JDBC_USERNAME = "root";
+    public static final String JDBC_PASSWORD = "abc";
+
+}
---
+++ src/Deposito.java
@@ -0,0 +1,22 @@
+
+public class Deposito {
+
+    private int id;
+    private String bank;
+    private String tgl_jto;
+    private String tgl_perpanjangan;
+    private String company;
+    private String bilyet1;
+    private String bilyet2;
+    private String type;
+    private double nilai;
+    private double bunga;
+    private int hari;
+    private double gross;
+    private double tax;
+    private double nett;
+    private double pokokdanBunga;
+    private String nama;
+    private Boolean status;
+
+}
